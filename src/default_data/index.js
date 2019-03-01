28b4273fe93f6d775dda2c77f9e1ce95b4355588
Kramerbaf <nds.kram@gmail.com>
2019-03-01 14:31:55 +0100

    changed langs

--- src/default_data/index.js
+++ src/default_data/index.js
@@ -2,10 +2,10 @@ module.exports = [
     {
         name: 'footer_A',
         storeName: {
-            en_GB: "footer_A"
+            en: "footer_A"
         },
         storeDesc: {
-            en_GB: ""
+            en: ""
         }
     }
 ]
